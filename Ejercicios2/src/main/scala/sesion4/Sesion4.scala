5d37b93c60ed5b43bd5f7c030ea02ff310552628
David Sotelo <davidsotelo@masmetodo.com>
2025-04-05 20:12:35 +0200

    Update BDProces 050425

--- Ejercicios2/src/main/scala/sesion4/Sesion4.scala
+++ Ejercicios2/src/main/scala/sesion4/Sesion4.scala
@@ -1,5 +1,6 @@
 package sesion4
 
+import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
@@ -10,4 +11,16 @@ object Sesion4 {
   def mapa(rdd: RDD[String])(implicit spark: SparkSession) = {
     rdd.map(s => (s, 1))
   }
+
+  def generarTublas(rdd: RDD[String]) = rdd.map(s => (s, 1))
+
+
+  def filtarImpar(rdd: RDD[Int]): RDD[Int] = rdd.filter((x => x % 2 != 0))
+
+  def listaIncremental(rdd: RDD[Int]): RDD[(Int)] = rdd.flatMap(x => List(x, x * 10))
+
+
+  def lecturaCsv(path: String)(sc: SparkContext): RDD[String] = sc.textFile(path)
+
+  def lecturaCsvDf(path: String)(implicit  spark: SparkSession) = spark.read.csv(path)
 }
--- Ejercicios2/src/test/scala/sesion4/Sesion4Test.scala
+++ Ejercicios2/src/test/scala/sesion4/Sesion4Test.scala
@@ -1,5 +1,6 @@
 package sesion4
 
+import org.apache.spark.rdd
 import org.apache.spark.rdd.RDD
 import sesion4.Sesion4._
 import utils.TestInit
@@ -10,10 +11,44 @@ class Sesion4Test extends TestInit{
 
   "Rdd" should "mapa" in {
 
-    val rdd:RDD[String] = sc.parallelize(Seq("a", "b", "c"))
+    val rdd:RDD[String] = sc.parallelize(Seq("a", "b", "c","d"))
     val out = mapa(rdd)
+    println(out.collect().mkString("::"))
+  }
+
+  "Rdd" should "generarTublas" in {
+
+
+    val rdd: RDD[String] = sc.parallelize(Seq("a","b","c"))
+    val out = generarTublas(rdd)
+
     println(out.collect().mkString("\n"))
   }
 
+  "Rdd" should "filtarImpar" in {
 
+    val rdd = sc.parallelize(Seq(1,2,3,4,5))
+    val out = filtarImpar(rdd)
+
+    println(out.collect() shouldBe Seq(1,3,5))
+  }
+  "Rdd" should "listaIncremental" in {
+
+    val rdd = sc.parallelize(Seq(1,2,3,4))
+    val out = listaIncremental(rdd)
+
+    out.collect() shouldBe Seq(1,10,2,20,3,30,4,40)
+  }
+
+"Lectura CSV" should "read" in {
+  val RDD = lecturaCsv("/Users/davidsoteloseguin/Library/Mobile Documents/com~apple~CloudDocs/Personal/Formacion /Bootcamp/Bootcamp KC/BD_Processing/BD_Process_Ejercicios/Ejercicios2/src/main/scala/ventas.csv")(sc)
+  RDD.take(5).foreach(println)
+
+}
+
+  "Lectura DF" should "read" in {
+    val df = lecturaCsvDf("/Users/davidsoteloseguin/Library/Mobile Documents/com~apple~CloudDocs/Personal/Formacion /Bootcamp/Bootcamp KC/BD_Processing/BD_Process_Ejercicios/Ejercicios2/src/main/scala/ventas.csv")
+    df.show(false)
+
+  }
 }
